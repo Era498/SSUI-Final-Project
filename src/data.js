698621fb9eddf7c6a8105ddf3593de2c66301f8b
Era498 <erinbeasley@swbell.net>
2020-11-29 23:32:09 -0500

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,143 @@
+export const dat = {
+    links: [
+      {
+        source: "A",
+        target: "B",
+      },
+      {
+        source: "A",
+        target: "C",
+      },
+      {
+        source: "A",
+        target: "D",
+      },
+      {
+        source: "A",
+        target: "E",
+      },
+      {
+        source: "A",
+        target: "F",
+      },
+      {
+        source: "A",
+        target: "G",
+      },
+      {
+        source: "B",
+        target: "D",
+      },
+      {
+        source: "B",
+        target: "E",
+      },
+      {
+        source: "B",
+        target: "F",
+      },
+      {
+        source: "B",
+        target: "G",
+      },
+      {
+        source: "C",
+        target: "E",
+      },
+      {
+        source: "C",
+        target: "F",
+      },
+      {
+        source: "C",
+        target: "G",
+      },
+      {
+        source: "D",
+        target: "F",
+      },
+      {
+        source: "D",
+        target: "G",
+      },
+      {
+        source: "F",
+        target: "G",
+      },
+      {
+        source: "A",
+        target: "H",
+      },
+      {
+        source: "B",
+        target: "I",
+      },
+      {
+        source: "C",
+        target: "J",
+      },
+      {
+        source: "D",
+        target: "K",
+      },
+      {
+        source: "E",
+        target: "L",
+      },
+      {
+        source: "F",
+        target: "M",
+      },
+      {
+        source: "G",
+        target: "N",
+      },
+    ],
+    nodes: [
+      {
+        id: "A",
+      },
+      {
+        id: "B",
+      },
+      {
+        id: "C",
+      },
+      {
+        id: "D",
+      },
+      {
+        id: "E",
+      },
+      {
+        id: "F",
+      },
+      {
+        id: "G",
+        color: "LightCoral"
+      },
+      {
+        id: "H",
+      },
+      {
+        id: "I",
+      },
+      {
+        id: "J",
+      },
+      {
+        id: "K",
+      },
+      {
+        id: "L",
+      },
+      {
+        id: "M",
+      },
+      {
+        id: "N",
+      },
+    ],
+  };
+
+  export default dat;
